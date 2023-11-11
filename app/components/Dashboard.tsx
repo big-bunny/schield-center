190c428742e79872e60c52f2d2d044e929c5dfaf
big-bunny <codecenter000@gmail.com>
2023-11-11 14:24:37 +0300

    blaze

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,87 +1,80 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Image from 'next/image';
 
-const Dashboard = ({ }) => {
+const Dashboard = () => {
   return (
-    <div className="min-h-screen p-8">
-      <h1 className="text-4xl p-3  text-white font-extrabold bg-gradient-to-r from-green-500 rounded-full mb-8">
+    <div className="min-h-screen p-8 bg-gray-100 mx-auto max-w-screen-lg">
+
+      <h1 className="text-2xl md:text-4xl p-3 text-white font-extrabold bg-gradient-to-r from-green-500 rounded-full mb-8">
         Welcome to Schield Centre Dashboard
       </h1>
 
-      {/* Use flexbox to align elements */}
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
+
         {/* Left section */}
-        <div className="flex flex-col mr-8">
+        <div className="md:w-1/2 md:mr-8">
           {/* Mission Statement */}
-          <div className="scrolling-text mb-4 text-center bg-white rounded-3xl">
-            <h3 className="text-2xl text-red-500">Mission Statement</h3>
-            <p className="text-xl p-2  text-black">
+          <div className="scrolling-text mb-4 text-center bg-white rounded-3xl p-4">
+            <h3 className="text-base md:text-lg lg:text-xl text-red-500">Mission Statement</h3>
+            <p className="text-xs md:text-base lg:text-lg p-2 text-black">
               Our mission is to provide access to quality education to children in Kajiado, Kenya, with the aim of empowering them to break the cycle of poverty and improve their lives and those of their families. We strive to create a learning environment that is supportive, inclusive, and empowering, valuing diversity and embracing the unique cultural heritage of our community.
             </p>
           </div>
 
           {/* Centered image */}
           <div className="flex justify-center mb-8">
-            <p className="text-3xl text-white">this image will be replaced with one of the late Schield`s</p>
+            <p className="text-xs md:text-lg lg:text-xl text-white mb-4 md:mb-0">This image will be replaced with one of the late Schield`s</p>
 
             <Image
               src="/images/gallery/construction.jpg"
               alt="Centered Image"
-              width={600}
-              height={400}
-              className="w-full"
+              width={300}
+              height={200}
+              className="w-full rounded-lg animate-bounce"
             />
           </div>
 
           {/* Vision Statement */}
-          <div className="scrolling-text mb-4 bg-white text-center rounded-3xl">
-            <h3 className="text-2xl text-red-500">Vision Statement</h3>
-            <p className="text-xl p-2 text-black">
+          <div className="scrolling-text mb-4 bg-white text-center rounded-3xl p-4">
+            <h3 className="text-base md:text-lg lg:text-xl text-red-500">Vision Statement</h3>
+            <p className="text-xs md:text-base lg:text-lg p-2 text-black">
               Our vision is a world where every child has access to quality education and the opportunity to reach their full potential. We believe that education is a fundamental human right and that it is essential for creating a more just and equitable society. At our center, we believe in fostering a culture of curiosity, creativity, and critical thinking, while also imparting life skills and values such as hard work, honesty, and respect for diversity. Our vision is to inspire and empower the next generation of global citizens who will make a positive impact on the world.
             </p>
           </div>
         </div>
 
         {/* Right section */}
-        <div className="flex-grow">
+        <div className="md:w-1/2 md:mt-0 mt-8">
           {/* Display important information */}
           <div className="bg-white rounded-lg p-4 mb-8">
-            <h2 className="text-xl font-bold mb-4">Important Information</h2>
-            <ul>
+            <h2 className="text-xs md:text-base lg:text-lg font-bold mb-4">Important Information</h2>
+            <ul className="text-xs md:text-sm lg:text-base">
               <li className="mb-2">Total Students: 210</li>
               <li className="mb-2">Total Teachers: 14</li>
             </ul>
           </div>
 
           {/* Display recent activities */}
-          <div className="bg-white rounded-lg p-4">
-            <h2 className="text-xl font-bold mb-4">Recent Activities</h2>
-            <ul>
+          <div className="bg-white rounded-lg p-4 mb-8">
+            <h2 className="text-xs md:text-base lg:text-lg font-bold mb-4">Recent Activities</h2>
+            <ul className="text-xs md:text-sm lg:text-base">
               <li className="mb-2">Trip to Court</li>
               <li className="mb-2">Sports Day</li>
             </ul>
           </div>
 
           {/* Live Calendar */}
-          <div className="bg-white rounded-lg p-4 mt-8">
-            <h2 className="text-xl font-bold mb-4">Upcoming Events</h2>
-            <p>
+          <div className="bg-white rounded-lg p-4">
+            <h2 className="text-xs md:text-base lg:text-lg font-bold mb-4">Upcoming Events</h2>
+            <p className="text-xs md:text-sm lg:text-base">
               This section will display a live calendar of upcoming events at the Schield Centre.
             </p>
           </div>
         </div>
+
       </div>
     </div>
   );
 };
 
 export default Dashboard;
-
-/* Comments */
-
-// This is the main heading for the dashboard.
-// The text is white and the background is a green gradient.
-// The heading has a rounded-full border radius.
-
-// This section contains the mission statement for the Schield Centre.
-//
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,4 @@
 "use client"
-// Header.js or Header.tsx
 import { useState } from 'react';
 import { UserButton, useUser } from '@clerk/nextjs';
 import Link from 'next/link';
@@ -14,24 +13,76 @@ const Header = () => {
     { text: 'About', url: '/about' },
     { text: 'Gallery', url: '/gallery' },
     { text: 'Donate', url: '/donate' },
-    { text: 'Team', url: '/team' }
+    { text: 'Team', url: '/team' },
   ];
 
   const programDropdown = [
-    { text: 'Child ', url: '/child' },
-    { text: ' Program', url: '/program' },
+    { text: 'Child', url: '/child' },
+    { text: 'Program', url: '/program' },
   ];
 
   return (
-    <header className="bg-gray-800 text-white py-4">
+    <header className="bg-gray-800 text-white py-4 relative z-50">
       <div className="container mx-auto flex items-center justify-between">
         <Link href="/" passHref legacyBehavior>
           <a className="text-2xl font-bold">Schieldcenter</a>
         </Link>
-        <nav className="space-x-4">
+
+        {/* Hamburger Menu */}
+        <div className="md:hidden">
+          <button
+            onClick={() => setDropdownOpen(!isDropdownOpen)}
+            className="text-2xl focus:outline-none"
+          >
+            &#9776;
+          </button>
+        </div>
+
+        {/* Dropdown for Small Screens */}
+        {isDropdownOpen && (
+          <div className="md:hidden absolute top-full mt-2 p-2 bg-gray-800 rounded-lg shadow-md z-50">
+            {menuItems.map((item, index) => (
+              <Link key={index} href={item.url} passHref legacyBehavior>
+                <a
+                  className="block text-white hover:text-gray-400 cursor-pointer py-2"
+                  onClick={() => setDropdownOpen(false)}
+                >
+                  {item.text}
+                </a>
+              </Link>
+            ))}
+            <Link href="#" passHref legacyBehavior>
+              <a
+                className="block text-white hover:text-gray-400 cursor-pointer py-2"
+                onClick={() => setDropdownOpen(false)}
+              >
+                Programs
+              </a>
+            </Link>
+            <ul className="mt-2">
+              {programDropdown.map((item, index) => (
+                <li key={index}>
+                  <Link href={item.url} passHref legacyBehavior>
+                    <a
+                      className="block text-white hover:text-gray-400 cursor-pointer py-2"
+                      onClick={() => setDropdownOpen(false)}
+                    >
+                      {item.text}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {/* Regular Navigation for Medium and Larger Screens */}
+        <nav className="hidden md:flex space-x-4 items-center">
           {menuItems.map((item, index) => (
             <Link key={index} href={item.url} passHref legacyBehavior>
-              <a className="hover:text-gray-400 cursor-pointer">{item.text}</a>
+              <a className="hover:text-gray-400 cursor-pointer">
+                {item.text}
+              </a>
             </Link>
           ))}
           <button
@@ -42,21 +93,14 @@ const Header = () => {
           >
             Programs
           </button>
-          {isDropdownOpen && (
-            <ul className="absolute mt-2 p-2 bg-gray-800 rounded-lg shadow-md">
-              {programDropdown.map((item, index) => (
-                <li key={index}>
-                  <Link href={item.url} passHref legacyBehavior>
-                    <a className="hover:text-gray-400 cursor-pointer">{item.text}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        
         </nav>
-        <div>
-          {isSignedIn ?  <UserButton afterSignOutUrl="/"/> : <UserGreeting />}
+
+        <div className="md:ml-4">
+          {isSignedIn ? (
+            <UserButton afterSignOutUrl="/" />
+          ) : (
+            <UserGreeting />
+          )}
         </div>
       </div>
     </header>
@@ -64,4 +108,3 @@ const Header = () => {
 };
 
 export default Header;
-
